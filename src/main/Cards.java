5cb73f76746ef5bdbe317c1f21aa04b0f6d445fb
Andrey Fomichev <andr7714@gmail.com>
2020-12-18 23:43:47 +0300

    images sizes added

--- src/main/Cards.java
+++ src/main/Cards.java
@@ -6,24 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cards {
-/*//- BufferedImage img = ImageIO.read(f); - зачитка картинки из файла
-//- ImageIO.write(img, "png", f); - запись картинки в файл
-//- img.getWidth(); img.getHeight(); - рамеры картинки
-//- BufferedImage img1 = img.getSubimage(x, y, w, h); - взятие области в картинке
-//- img.getRGB(x, y); - взятие цвета точки по координате
-//- Color c = new Color(img.getRGB(x, y)); c.getRed(); c.getGreen(); c.getBlue(); c.equals(c1) - работа с цветом точки
-Если нужно ставить масть в HTML коде, то комбинация будет следующей:
-    Для пики - &#9824;
-    Для черви - &#9829;
-    Для бубны - &#9830;
-    Для трефы - &#9827;
-*/
-    private BufferedImage img;
+    /* BufferedImage img = ImageIO.read(f); - зачитка картинки из файла
+       ImageIO.write(img, "png", f); - запись картинки в файл
+       img.getWidth(); img.getHeight(); - рамеры картинки
+       BufferedImage img1 = img.getSubimage(x, y, w, h); - взятие области в картинке
+       img.getRGB(x, y); - взятие цвета точки по координате
+       Color c = new Color(img.getRGB(x, y)); c.getRed(); c.getGreen(); c.getBlue(); c.equals(c1) - работа с цветом точки
+    Если нужно ставить масть в HTML коде, то комбинация будет следующей:
+        Для пики - &#9824;
+        Для черви - &#9829;
+        Для бубны - &#9830;
+        Для трефы - &#9827;
+    */
+    private static BufferedImage img;
+    private static File currFile;
 
-    private void saveImage(File f, String name, int x, int y, int w, int h) throws IOException {
-        img = ImageIO.read(f);  //- зачитка картинки из файла
-        ImageIO.write(img, "png", new File("c:\\1\\b.png")); //- запись картинки в файл
-
+    private static void saveImage(String name, int x, int y, int w, int h) throws IOException {
+        BufferedImage b1 = img.getSubimage(x, y,w, h); //- взятие области в картинке
+        ImageIO.write(b1, "png", new File("c:\\1\\" + name + ".png")); //- запись картинки в файл
     }
 
     public static void main(String[] args) throws IOException {
@@ -34,26 +34,35 @@ public class Cards {
         String path = args[0];
 
         File dir = new File(path); //path указывает на директорию
+        if (dir.listFiles() ==null || dir.listFiles().length ==0) {
+            System.out.println("No files found in path: " + path);
+            System.exit(1);
+        }
         List<File> lst = new ArrayList<>();
+
         for ( File file : dir.listFiles() ){
             if ( file.isFile() )
                 lst.add(file);
-
-        }
+       }
         //for (File f:lst)
-        File f = lst.get(0);
+        currFile = lst.get(0);
         {
-            saveImage(f, )
-            BufferedImage b1 = img.getSubimage(152, 591,32, 24); //- взятие области в картинке
-            ImageIO.write(b1, "png", new File("c:\\1\\b1.png")); //- запись картинки в файл
-            BufferedImage b1 = img.getSubimage(152, 591,32, 24); //- взятие области в картинке
+            img = ImageIO.read(currFile); //зачитка картинки из файла
+            saveImage("0image", 0,0,img.getWidth(), img.getHeight());
+            saveImage("a1",145, 591,34, 24); //- взятие области в картинке
+            saveImage("a2",169, 634,33, 33); //- взятие области в картинке
 
+            saveImage("b1",217, 591,34, 24); //- взятие области в картинке
+            //saveImage("a2",217, 591,34, 24); //- взятие области в картинке
 
+            saveImage("c1",289, 591,34, 24); //- взятие области в картинке
+            //saveImage("a3",289, 591,34, 24); //- взятие области в картинке
+            saveImage("d1",361, 591,34, 24); //- взятие области в картинке
+            saveImage("e1",433, 591,34, 24); //- взятие области в картинке
 
-            BufferedImage b2 = img.getSubimage(224, 591,32, 24); //- взятие области в картинке
-            ImageIO.write(b2, "png", new File("c:\\1\\b2.png")); //- запись картинки в файл
-            BufferedImage b3 = img.getSubimage(289, 591,32, 24); //- взятие области в картинке
-            ImageIO.write(b3, "png", new File("c:\\1\\b3.png")); //- запись картинки в файл
+            //saveImage("b1",152, 591,17, 24); //- взятие области в картинке
+            //saveImage("b2",224, 591,34, 24); //- взятие области в картинке
+            //saveImage("b3",289, 591,34, 24); //- взятие области в картинке
 
         }
 
