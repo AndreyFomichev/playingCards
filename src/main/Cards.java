8df8ca64e5f4d6c751494488e7f5ad4f78ffc501
Andrey Fomichev <andr7714@gmail.com>
2020-12-21 02:52:26 +0300

    final changes

--- src/main/Cards.java
+++ src/main/Cards.java
@@ -9,9 +9,8 @@ public class Cards {
     private static int cntBg1, maxErrors = 20, bgColor, xx, yy ;
     private static String encoded = "", replacementStr = "";
     private static BufferedImage img, img_clone;
-    private static String[][] symbols = {{ "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9", "1",  "A",  "J",  "Q", "K",  "♠", "♣", "♥", "♦" },
-                                         {"20", "10", "20", "10", "20", "15", "14", "20", "14", "20", "11", "20", "15", "s50", "c50", "h50", "d30"  } };
-
+    private static String[][] symbols = {{ "2", "6", "8",   "3",  "4",   "5",  "7",  "9",  "1",  "A", "J",  "Q",  "K",  "♦",   "♣",   "♠",   "♥", },
+                                         {"15", "8", "16", "17", "20",  "12", "10", "12", "12", "20", "8", "25", "15", "d9", "c50", "s46", "h50",  } };
     private static File dir;
 
     private static int pixelsCount(BufferedImage image){
@@ -23,50 +22,45 @@ public class Cards {
         return  cnt;
     }
 
-    private static String findSymbol(String symbol, int num) {
+    private static String findSymbol(String symbol, int num) throws IOException, InterruptedException {
         int cntBg2;
-        for (int deltax = 0; deltax <= 24; deltax++) {
-            Graphics2D g = img_clone.createGraphics();
-            g.drawImage(img, 0, 0, null);
-            //g.setFont(g.getFont().deriveFont(32f)); //Arson-Pro-Medium-otf
-            g.setFont(new Font("TJesterday Demo Regular", Font.PLAIN, (replacementStr.length() == 0 ? 30 : 46)));
-            g.setColor(Color.GREEN);
-            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            g.drawString(String.valueOf(symbol), xx - 2 + deltax + (replacementStr.length() == 0 ? 0 : 22)
-                    , yy + 23 + (replacementStr.length() == 0 ? 0 : 54));
-            g.dispose();
-            cntBg2 = pixelsCount(img_clone);
-            if (Math.abs(cntBg1 - cntBg2) <= maxErrors && cntBg1 > 500) {
-                encoded = encoded + (replacementStr.length() == 0 ? symbol : replacementStr);
-                System.out.println("FOUND symbol=" + symbol + ", num = " + num + ", xx=" + (xx + deltax) + ", cntBg1=" + cntBg1 + ", cntBg2=" + cntBg2 + ", diff=" + (cntBg1 - cntBg2) + ", bgcolor=" + new Color(bgColor).toString() + ", deltax=" + deltax);
-                //..File f = new File(dir + "\\buf"+symbol+".png");
-                //ImageIO.write(img_clone, "png", f);
-                //      Desktop dt = Desktop.getDesktop();
-                //  dt.open(f);
-                //Thread.sleep(1000);
-                return symbol;
+        for (int deltay = -2; deltay <=+1; deltay++) {
+            for (int deltax = 0; deltax <= 15; deltax++) {
+                Graphics2D g = img_clone.createGraphics();
+                g.drawImage(img, 0, 0, null);
+                g.setFont(new Font("Jesterday Demo Regular", Font.PLAIN, (replacementStr.length() == 0 ? 30 : 46)));
+                g.setColor(Color.GREEN);
+                g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+                g.drawString(String.valueOf(symbol), xx - 2 + deltax + (replacementStr.length() == 0 ? 0 : 22)
+                        , yy + 23 + deltay + (replacementStr.length() == 0 ? 0 : 54));
+                g.dispose();
+                cntBg2 = pixelsCount(img_clone);
+                if (Math.abs(cntBg1 - cntBg2) <= maxErrors && cntBg1 > 500) {
+                    encoded = encoded + (replacementStr.length() == 0 ? symbol : replacementStr);
+                 // System.out.println("FOUND symbol=" + (replacementStr.length() == 0 ? symbol : replacementStr) + ", num = " + num + ", xx=" + (xx + deltax) + ", cntBg1=" + cntBg1 + ", cntBg2=" + cntBg2 + ", diff=" + (cntBg1 - cntBg2) + ", bgcolor=" + new Color(bgColor).toString() + ", deltax=" + deltax);
 
-            } else {
-                //if (String.valueOf(symbol).equals("♣") ) {
-                if ( //replacementStr.length() > 0  //||
-                        symbol.equals("0")
-                                || symbol.equals("1")
-                ) {
-                    System.out.println("symbol=" + symbol + ", num = " + num + ", xx=" + (xx + deltax) + ", cntBg1=" + cntBg1 + ", cntBg2=" + cntBg2 + ", diff=" + (cntBg1 - cntBg2) + ", bgcolor=" + new Color(bgColor).toString() + ", deltax=" + deltax);
-                    //     File f = new File(dir + "\\buf.png");
-                    //     ImageIO.write(img_clone, "png", f);
-                    //     Desktop dt = Desktop.getDesktop();
-                    //     dt.open(f);
-                    //     Thread.sleep(1000);
+                    return symbol;
+
+                } else {
+                    //if (String.valueOf(symbol).equals("♣") ) {
+                    if ( //replacementStr.length() > 0  //||
+                            //replacementStr.equals("3")
+                                     symbol.equals("3")
+                    ) {
+               //         System.out.println("symbol=" + symbol + ", num = " + num + ", xx=" + (xx + deltax) + ", cntBg1=" + cntBg1 + ", cntBg2=" + cntBg2 + ", diff=" + (cntBg1 - cntBg2) + ", bgcolor=" + new Color(bgColor).toString() + ", deltax=" + deltax);
+                  //      File f = new File(dir + "\\buf.png");
+                    //    ImageIO.write(img_clone, "png", f);
+                      //  Desktop dt = Desktop.getDesktop();
+                    //    dt.open(f);
+                    //    Thread.sleep(1000);
+                    }
                 }
-
             }
         }
         return "";
     }
 
-    private static void decodeCard(int num) {
-        encoded = encoded.concat("/");
+    private static void decodeCard(int num) throws IOException, InterruptedException {
         xx = 143 + 72 * num;
         yy = 591;
         int symbolsFound = 0;
@@ -90,13 +84,13 @@ public class Cards {
                 encoded = encoded.concat("0");
             if (s.length() > 0 )
                 symbolsFound++;
-
         }
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         if (args.length ==0 ) {
             System.out.println("Файл run.bat параметром принимает путь до папки с картинками");
+            System.exit(1);
         }
         String path = args[0];
         dir = new File(path); //path указывает на директорию
@@ -111,7 +105,6 @@ public class Cards {
                 lst.add(file);
 
         for (File currFile:lst) {
-            if (!currFile.getName().equals("6s5h10h.png")) continue;
             encoded = "";
             img = ImageIO.read(currFile); //зачитка картинки из файла
             img_clone = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
@@ -119,9 +112,9 @@ public class Cards {
             decodeCard(0);
             decodeCard(1);
             decodeCard(2);
-            //decodeCard(3);
-            //decodeCard(4);
-            System.out.println(currFile.getName() + " - " + encoded);
+            decodeCard(3);
+            decodeCard(4);
+            System.out.println(currFile.getName() + " - " + encoded + (currFile.getName().substring(0,currFile.getName().indexOf('.')).equals(encoded) ? " (+)":"" ));
         }
     }
 }
